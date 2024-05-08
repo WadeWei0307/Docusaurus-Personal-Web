cd52927fa28168b333c6e3b038e4392628347ccb
WadeWei0307 <a0926151140@gmail.com>
2024-05-08 20:32:16 +0800

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docusaurus-personal-p6b9zti96-wadewei0307s-projects.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
